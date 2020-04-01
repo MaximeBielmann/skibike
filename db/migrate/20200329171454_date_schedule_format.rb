e90943648e065abe8c9c94c7da0538d149b0390c
EC2 Default User <ec2-user@ip-172-31-30-239.us-east-2.compute.internal>
2020-04-01 09:22:31 +0000

    try to fix some shit in migrations

--- db/migrate/20200329171454_date_schedule_format.rb
+++ db/migrate/20200329171454_date_schedule_format.rb
@@ -1,5 +1,9 @@
 class DateScheduleFormat < ActiveRecord::Migration[5.2]
-  def change
-    change_column :schedules, :schedule_date, :string
-  end
+    def up
+      change_column :schedules, :schedule_date, :datetime
+    end
+
+    def down
+      change_column :schedules, :schedule_date, :string
+    end
 end
--- db/migrate/20200329171757_restore_date_schedule_format.rb
+++ db/migrate/20200329171757_restore_date_schedule_format.rb
@@ -1,5 +1,9 @@
 class RestoreDateScheduleFormat < ActiveRecord::Migration[5.2]
-  def change
-    change_column :schedules, :schedule_date, :datetime
-  end
+    def up
+      change_column :schedules, :schedule_date, :datetime
+    end
+
+    def down
+      change_column :schedules, :schedule_date, :datetime
+    end
 end
