7d390c3da1d1d96e1a6ad80e7565c23e493dce2e
EC2 Default User <ec2-user@ip-172-31-30-239.us-east-2.compute.internal>
2020-04-10 12:46:46 +0000

    fix domain in mailgun config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
     api_key: ENV["MAILGUN_API_KEY"],
-    domain: 'skibike-quebec.herokuapp.com',
+    domain: 'https://skibike-quebec.herokuapp.com',
     # api_host: 'api.eu.mailgun.net'  # Uncomment this line for EU region domains
   }
 end
